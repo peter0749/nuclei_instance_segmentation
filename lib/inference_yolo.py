bd420406347f1b49e047bea06ab11b9c6589a2c5
jengku <jengku@gmail.com>
2018-02-22 18:05:51 +0800

    fix module import bugs

--- lib/inference_yolo.py
+++ lib/inference_yolo.py
@@ -7,7 +7,7 @@ session = tf.Session(config=tfconfig)
 import keras
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 import models
-import reader
+from reader import dataset_filepath, dir_reader
 import config as conf
 from sklearn.model_selection import train_test_split
 from utils import normalize
@@ -19,8 +19,8 @@ print('Loading trained weights...')
 yolo_model.load_weights(conf.YOLO_CKPT)
 
 print('Generating metadata...')
-imgs_meta  = reader.dataset_filepath(conf.TEST_PATH, get_masks=False)
-imgs_batch, imgs_original, imgs_path = reader.dir_reader(imgs_meta)
+imgs_meta  = dataset_filepath(conf.TEST_PATH, get_masks=False)
+imgs_batch, imgs_original, imgs_path = dir_reader(imgs_meta)
 
 netouts = yolo_model.predict([imgs_batch, imgs_meta], batch_size=conf.YOLO_BATCH_SIZE)
 del imgs_batch
