bdd7be0da28dba1dee5df3d15bf0ae85fe25aafa
jengku <jengku@gmail.com>
2018-02-22 18:22:46 +0800

    fix reader bugs

--- lib/reader.py
+++ lib/reader.py
@@ -4,7 +4,6 @@ from tqdm import tqdm
 import glob
 from get_image_size import get_image_size
 from skimage.measure import regionprops
-from skimage.io import imread
 import copy
 import cv2
 '''
@@ -28,7 +27,7 @@ def dataset_filepath(root, get_masks=True):
         if get_masks:
             image['masks'] = []
             for mask_path in glob.glob(subdir+'/masks/*.png'):
-                mask = imread(mask_path, as_grey=True)
+                mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
                 assert mask.ndim == 2
                 (ymin, xmin, ymax, xmax) = regionprops((mask>0).astype(np.int32), cache=True)[0].bbox
                 image['masks'].append({'mask': mask_path, 'ymin': ymin, 'xmin': xmin, 'ymax': ymax, 'xmax': xmax}) # get bounding box
@@ -42,7 +41,7 @@ def dir_reader(img_meta_wo_markers):
     original_images  = []
     image_filenames  = []
     for i, img_meta in tqdm(enumerate(img_meta_wo_markers), total=l):
-        img = imread(img_meta['image'])[...,:3] # only BGR channels
+        img = cv2.imread(img_meta['image'], cv2.IMREAD_COLOR)[...,:3] # only BGR channels
         img = img[...,::-1] # BGR -> RGB
         img_input = cv2.resize(img, (conf.YOLO_DIM, conf.YOLO_DIM))
         img_input = img_input / 255.
