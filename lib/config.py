e1d637ce8bd282aff1b987b11b00396b0db0772d
peter0749 <jengku@gmail.com>
2018-02-22 22:10:47 +0800

    lower threshold

--- lib/config.py
+++ lib/config.py
@@ -1,7 +1,7 @@
 # ROOT TO DATASET (DATA & TEST)
 DATA_PATH = '/hdd/dataset/nuclei_dataset/stage1_train' # this will split into training/validation
 TEST_PATH = '/hdd/dataset/nuclei_dataset/stage1_test'
-VALID_SPLIT = 0.1
+VALID_SPLIT = 65
 
 # U-Net for semantic segmentation
 U_NET_DIM = 64
@@ -12,14 +12,14 @@ U_NET_OUT_DIR = '/hdd/dataset/nuclei_dataset/unet_out'
 YOLO_DIM = 416
 YOLO_GRID= 13
 YOLO_BOX = 5
-OBJECT_THRESHOLD = 0.6 # <- notice here
+OBJECT_THRESHOLD = 0.3 # <- notice here
 NMS_THRESHOLD = 0.3
 # ANCHORS = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828]
 ANCHORS = [1.08,1.19,  3.42,4.41,  6.63,11.38,  9.42,5.11,  16.62,10.52]
 NO_OBJECT_SCALE  = 1.0
 OBJECT_SCALE     = 5.0
 COORD_SCALE      = 1.0
-WARM_UP_BATCHES  = 3
+WARM_UP_BATCHES  = 0 
 TRUE_BOX_BUFFER  = 50
 BOX = 5
 
