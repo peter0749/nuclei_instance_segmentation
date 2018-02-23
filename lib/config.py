92036272faf7251b70740853e30ce3b50315efe7
jengku <jengku@gmail.com>
2018-02-23 14:52:10 +0800

    now time to train U-net to segment

--- lib/config.py
+++ lib/config.py
@@ -14,12 +14,13 @@ YOLO_GRID= 13
 YOLO_BOX = 5
 OBJECT_THRESHOLD = 0.2 # <- notice here
 NMS_THRESHOLD = 0.2
+U_NET_THRESHOLD = 0.5
 # ANCHORS = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828]
 ANCHORS = [1.08,1.19,  3.42,4.41,  6.63,11.38,  9.42,5.11,  16.62,10.52]
 NO_OBJECT_SCALE  = 1.0
 OBJECT_SCALE     = 5.0
 COORD_SCALE      = 1.0
-WARM_UP_BATCHES  = 0 
+WARM_UP_BATCHES  = 0
 TRUE_BOX_BUFFER  = 50
 BOX = 5
 
@@ -27,26 +28,38 @@ YOLO_DRAW_LINE_W = 1
 YOLO_SHOW_CONF = False
 
 YOLO_USE_MULTI_GPU=2
+U_NET_USE_MULTI_GPU=2
+
 YOLO_BATCH_SIZE=48 ## each gpus's batch size = YOLO_BATCH_SIZE / YOLO_USE_MULTI_GPU
 U_NET_BATCH_SIZE=8
+
 GENERATOR_WORKERS=4
 
 YOLO_EPOCHS=500
 U_NET_EPOCHS=300
 
 YOLO_CKPT = '/hdd/dataset/nuclei_dataset/yolo.h5'
+U_NET_CKPT = '/hdd/dataset/nuclei_dataset/unet.h5'
 
 YOLO_TFBOARD_DIR = '/hdd/dataset/nuclei_dataset/yolo_tfboard'
+U_NET_TFBOARD_DIR = '/hdd/dataset/nuclei_dataset/unet_tfboard'
 YOLO_OPT_ARGS = {
     'lr'              : 2e-4,
     'clipvalue'       : 0.1 ,
     'clipnorm'        : 1.0 ,
 }
+U_NET_OPT_ARGS = {
+    'lr'              : 1e-3,
+}
+
 YOLO_MIN_LOSS = 0
 YOLO_MAX_LOSS = 10 # This prevent nans. If your loss is not chaning, then set a higher value.
 
 YOLO_EARLY_STOP = 50
+U_NET_EARLY_STOP = 50
+
 YOLO_OUT_DIR = '/hdd/dataset/nuclei_dataset/detection_output'
+U_NET_OUT_DIR = '/hdd/dataset/nuclei_dataset/detection_output'
 
 ### !!! DO NOT EDITING THE CONFIGURATION BELOW !!! ###
 
@@ -59,4 +72,10 @@ yolo_generator_config = {
     'ANCHORS'         : ANCHORS,
     'BATCH_SIZE'      : YOLO_BATCH_SIZE,
     'TRUE_BOX_BUFFER' : TRUE_BOX_BUFFER,
+}
+
+unet_generator_config = {
+    'IMAGE_H'         : U_NET_DIM,
+    'IMAGE_W'         : U_NET_DIM,
+    'BATCH_SIZE'      : U_NET_BATCH_SIZE,
 }
--- lib/generators.py
+++ lib/generators.py
@@ -75,13 +75,11 @@ class YOLO_BatchGenerator(Sequence):
             r_bound = len(self.images)
             l_bound = r_bound - self.config['BATCH_SIZE']
 
-        instance_count = 0
-
         x_batch = np.zeros((r_bound - l_bound, self.config['IMAGE_H'], self.config['IMAGE_W'], 3))                         # input images
         b_batch = np.zeros((r_bound - l_bound, 1     , 1     , 1    ,  self.config['TRUE_BOX_BUFFER'], 4))   # list of self.config['TRUE_self.config['BOX']_BUFFER'] GT boxes
         y_batch = np.zeros((r_bound - l_bound, self.config['GRID_H'],  self.config['GRID_W'], self.config['BOX'], 4+1))                # desired network output
 
-        for train_instance in self.images[l_bound:r_bound]:
+        for instance_count, train_instance in enumerate(self.images[l_bound:r_bound]):
             # augment input image and fix object's position and size
             img, all_objs = self.aug_image(train_instance, jitter=self.jitter)
 
@@ -133,9 +131,6 @@ class YOLO_BatchGenerator(Sequence):
 
             # assign input image to x_batch
             x_batch[instance_count] = self.norm(img)
-
-            # increase instance counter in current batch
-            instance_count += 1
 
         return [x_batch, b_batch], y_batch
 
@@ -196,3 +191,116 @@ class YOLO_BatchGenerator(Sequence):
 
         return image, all_objs
 ### end YOLO generator
+
+### U-Net generator ###
+class U_NET_BatchGenerator(Sequence):
+    def __init__(self, images,
+                       config,
+                       shuffle=True,
+                       jitter=True,
+                       norm=None):
+        self.generator = None
+
+        self.images = [] # pairs of (img, mask)
+        self.config = config
+
+        self.shuffle = shuffle
+        self.jitter  = jitter
+        self.norm    = norm
+
+        ### augmentors by https://github.com/aleju/imgaug
+        sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+
+        # Define our sequence of augmentation steps that will be applied to every image
+        # All augmenters with per_channel=0.5 will sample one value _per image_
+        # in 50% of all cases. In all other cases they will sample new values
+        # _per channel_.
+        self.aug_pipe = iaa.Sequential(
+            [
+                iaa.Fliplr(0.5),
+                iaa.Flipud(0.5),
+                sometimes(iaa.Affine(
+                    rotate=(-45, 45), # rotate by -45 to +45 degrees
+                    shear=(-16, 16), # shear by -16 to +16 degrees
+                    order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
+                    cval=0, # if mode is constant, use a cval between 0 and 255
+                    mode='constant' # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+                )),
+                sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
+                sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05)))
+            ],
+            random_order=True
+        )
+
+        for img in images:
+            for mask in copy.deepcopy(img['masks']):
+                mask['image'] = img['image']
+                mask['width'] = img['width']
+                mask['height'] = img['height']
+                self.images.append(mask) # list of: {'image':filepath, 'mask':filepath, width, height, xmax, ymax, ...}
+
+        if self.shuffle: np.random.shuffle(self.images)
+
+    def __len__(self):
+        return int(np.ceil(float(len(self.images))/self.config['BATCH_SIZE']))
+
+    def __getitem__(self, idx):
+        l_bound = idx*self.config['BATCH_SIZE']
+        r_bound = (idx+1)*self.config['BATCH_SIZE']
+
+        if r_bound > len(self.images):
+            r_bound = len(self.images)
+            l_bound = r_bound - self.config['BATCH_SIZE']
+
+        x_batch = np.zeros((r_bound - l_bound, self.config['IMAGE_H'], self.config['IMAGE_W'], 3))                         # input images
+        y_batch = np.zeros((r_bound - l_bound, self.config['IMAGE_H'], self.config['IMAGE_W'], 1))                # desired network output
+
+        for instance_count, train_instance in enumerate(self.images[l_bound:r_bound]):
+            # augment input image and fix object's position and size
+            img, lab = self.aug_image(train_instance, jitter=self.jitter)
+
+            x_batch[instance_count,...]   = self.norm(img)
+            y_batch[instance_count,...,0] = lab
+
+        return x_batch, y_batch
+
+    def on_epoch_end(self):
+        if self.shuffle: np.random.shuffle(self.images)
+
+    def aug_image(self, train_instance, jitter):
+        image_name = train_instance['image']
+        image = cv2.imread(image_name, cv2.IMREAD_COLOR)[...,:3]
+        assert image is not None
+        image = image[...,::-1] ## BGR -> RGB
+
+        mask_name = train_instance['mask']
+        mask  = cv2.imread(mask_name, cv2.IMREAD_GRAYSCALE)
+        assert mask is not None
+        mask  = np.expand_dims(mask, -1)
+
+        ymin, ymax = train_instance['ymin'], train_instance['ymax']
+        xmin, xmax = train_instance['xmin'], train_instance['xmax']
+
+        if jitter:
+            croph, cropw = np.random.uniform(0.9, 1.1)*(ymax-ymin), np.random.uniform(0.9, 1.1)*(xmax-xmin) ## random scale
+            xmin = int(np.random.uniform(-0.1, 0.1) * cropw) + xmin ## random crop
+            ymin = int(np.random.uniform(-0.1, 0.1) * croph) + ymin
+            xmax = xmin+cropw
+            ymax = ymin+croph
+
+        image = image[ymin:ymax, xmin:xmax]
+        mask  =  mask[ymin:ymax, xmin:xmax]
+
+        h, w, c = image.shape
+
+        if jitter:
+            seq_det = self.aug_pipe.to_deterministic()
+            image = seq_det.augment_image(image)
+            mask  = seq_det.augment_image( mask)
+
+        # resize the image to standard size
+        image = cv2.resize(image, (self.config['IMAGE_H'], self.config['IMAGE_W'])) # shape: (IMAGE_H, IMAGE_W, 3)
+        mask  = cv2.resize(np.squeeze(mask) , (self.config['IMAGE_H'], self.config['IMAGE_W'])) # shape: (IMAGE_H, IMAGE_W)
+
+        return image, mask
+### end U-Net generator ###
