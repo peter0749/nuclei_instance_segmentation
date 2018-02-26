653f8b87a6adadff3b0c15eb97fbba672b3a128e
peter0749 <jengku@gmail.com>
2018-02-26 09:04:48 +0800

    more imgaug

--- lib/generators.py
+++ lib/generators.py
@@ -219,15 +219,22 @@ class U_NET_BatchGenerator(Sequence):
             [
                 iaa.Fliplr(0.5),
                 iaa.Flipud(0.5),
+                sometimes(iaa.CropAndPad(
+                    percent=(-0.05, 0.1),
+                    pad_mode='reflect',
+                    pad_cval=0
+                )),
                 sometimes(iaa.Affine(
+                    scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, individually per axis
+                    translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
                     rotate=(-45, 45), # rotate by -45 to +45 degrees
                     shear=(-16, 16), # shear by -16 to +16 degrees
                     order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
                     cval=0, # if mode is constant, use a cval between 0 and 255
-                    mode='constant' # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+                    mode='reflect' # use any of scikit-image's warping modes (see 2nd image from the top for examples)
                 )),
                 sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
-                sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05)))
+                sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))), 
             ],
             random_order=True
         )
